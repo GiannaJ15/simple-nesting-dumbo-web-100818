06a526802c59b95b8021918d9f2844cb6ebe372d
gianna.jolibeth15 <gianna.jolibeth15@gmail.com>
2018-10-01 19:23:55 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -85,7 +85,7 @@ def adding_matz
         }
      }
     
-matz = {:yukihiro_matsumoto =>{:known_for => "Ruby",:languages => ["LISP", "C"]}.merge(programmer_hash)
+matz = {:yukihiro_matsumoto => {:known_for => "Ruby",:languages => ["LISP", "C"]}}.merge(programmer_hash)
 
     
 end
